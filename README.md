a247b90c5bcd2dd4d4e2b5832944880a2fa8c1ef
Daniel Gross <dan.12sq@gmail.com>
2018-06-22 00:18:54 -0400

    input/output file selection, frame sample rate
    output fps, temp image directory and cleanup,
    improved time formatting

--- README.md
+++ README.md
@@ -1,15 +1,30 @@
-Simple script to create a timelapse
+# Simple script to create a timelapse
 
-Takes one frame per second from input file.
-Creates the output video at 30 fps.
-
-Assumes the input file is `D:/test.mov` and
-output file is `D:/test_out.mp4`
+Takes a specified number of frames per second from input file.
+Creates the output video at a specified fps.
+Suggest defaults are 1 frame per second from the input file
+and 30 FPS in the output video.
 
 Uses Python3 and MoviePy
 
-Upcoming features:
+## Upcoming features
 
-* More appropriate default quality and customizable quality
-* Customizable input/output files
 * Allowing multiple input files to one unified output file
+* Rounding on times given
+* Code cleanup/refactoring
+
+## Examples
+
+This script was used to produce the time lapse videos below:
+
+[Source footage](https://youtu.be/femwl6gyXmw)
+
+[1 frame per 1s of source footage, 30fps output](https://youtu.be/6whVmfM32bM)
+
+[1 frame per 2s of source footage, 30fps output](https://youtu.be/o--FmR5Mukw)
+
+[1 frame per 4s of source footage, 30fps output](https://youtu.be/FbA2oVj7Yh8)
+
+[1 frame per 8s of source footage, 30fps output](https://youtu.be/ZCyjla6Yr08)
+
+[1 frame per 16s of source footage, 30fps output](https://youtu.be/ITFrZJOJg6U)
--- timelapse.py
+++ timelapse.py
@@ -1,42 +1,79 @@
 #!/usr/bin/python3.6
 
-from moviepy.editor import *
+from moviepy.editor import VideoFileClip, ImageSequenceClip
 import os
+import shutil
+import sys
+import tempfile
 import time
 
-clip_name = "D:/test.mov"
+def formatted_remaining(rem):
+    s_rem = '{}:{}'.format(int(rem/60), rem%60)
+    if s_rem[-2] == ':':
+        s_rem = s_rem[:-1] + '0' + s_rem[-1]
+    return s_rem
+
+# get input/output files
+clip_name = ''
+while clip_name == '':
+    clip_name = input('Path to clip: ')
+    if not os.path.exists(clip_name):
+        print('Clip does not exit at given path: {}'.format(clip_name))
+        clip_name = ''
+output_name = input('Output clip path and name: ')
+if not output_name.endswith('.mp4'):
+    print('Output must be an mp4 file')
+    sys.exit(1)
+
+# general settings
+extract_f_per_x_s = int(input('Extract 1 frame per x second of input video: '))
+output_fps = int(input('FPS in output video: '))
+directory = tempfile.mkdtemp()
+print('Extracted frames will be stored in {}'.format(directory))
+
 clip = VideoFileClip(clip_name)
 frame_count = int(clip.end) + 1
+frame_count = int(frame_count / extract_f_per_x_s)
 print("Extracting {} frames from {}".format(frame_count, clip_name))
 frame_digits = len(str(frame_count))
 
+# extract frames
 start_time = time.time()
 rolling = []
-for t in range(1, frame_count + 1):
-    zeros = ''.join('0' for i in range(frame_digits - len(str(t))))
-    frame_name = zeros + str(t)
-    if t == 1 or t % 5 == 0:
+image_files = []
+index = 0
+for t in range(1, frame_count*extract_f_per_x_s + 1, extract_f_per_x_s):
+    index += 1
+    zeros = ''.join('0' for i in range(frame_digits - len(str(index))))
+    frame_name = zeros + str(index)
+    if index == 1 or index % 5 == 0:
         print("Saving frame {}".format(frame_name))
+    frame_name = os.path.join(directory, "test_image{}.png".format(frame_name))
     frame_start = time.time()
-    image = clip.save_frame("D:/images/test_image{}.png".format(frame_name), t=t-1)
+    image = clip.save_frame(frame_name, t=t-1)
     elapsed_time = time.time() - frame_start
+    image_files.append(frame_name)
 
     if len(rolling) > frame_count / 10 and len(rolling) > 5:
         # keep the rolling average based on up to the last 10% of the input video
         rolling = rolling[1:]
     rolling.append(elapsed_time)
-    if t == 1 or t % 10 == 0:
+    if index == 1 or index % 5 == 0:
         # print rolling average and est. time remaining every 10 frames
         avg = sum(rolling)/float(len(rolling))
         print("Average extraction time per frame: {}".format(avg))
-        rem = int((frame_count - t) * avg)
-        print("Estimated remaining time: {}s".format(rem))
-print("Total elapsed time for extraction: {}s".format(int(time.time() - start_time)))
-
-frames = 30
-directory = 'D:/images/'
+        s_rem = formatted_remaining(int((frame_count - index) * avg))
+        print("Estimated remaining time: {}".format(s_rem))
+total_time = formatted_remaining(int(time.time() - start_time))
+print("Total elapsed time for extraction: {}".format(total_time))
 
 # stitch images together into output video
-names = [directory + name for name in os.listdir(directory) if name[-4:] == ".png"]
-clip = ImageSequenceClip(names, fps = frames)
-clip.write_videofile('D:/test_out.mp4', fps = frames)
+clip = ImageSequenceClip(image_files, fps = output_fps)
+clip.write_videofile(output_name, fps = output_fps)
+
+# cleanup
+answer = ''
+while answer not in set('ynYN'):
+    answer = input('Erase image frame files? ')
+if answer in set('yY'):
+    shutil.rmtree(directory)
